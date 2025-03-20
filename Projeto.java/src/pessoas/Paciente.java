604d3b3cda0f43f2143947e89fcc013301e8f998
João Carlos <justodeandrade@gmail.com>
2025-03-19 21:27:49 -0300

    Projeto de mini Sistema de cadastro

---
+++ Projeto.java/src/pessoas/Paciente.java
@@ -0,0 +1,11 @@
+package pessoas;
+
+
+public class Paciente {
+    public String nome;
+    public int idade;
+    public double peso;
+    public double altura;
+    public char genero;
+    public String convenio;
+}
---
+++ Projeto.java/src/sistema/App.java
@@ -0,0 +1,42 @@
+package sistema;
+import java.util.Scanner;
+import java.util.Locale;
+import pessoas.Paciente;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        Paciente ps;
+        ps = new Paciente();
+        char resp;
+        String tem_convenio;
+
+        do{
+            System.out.print("Digite o seu nome: ");
+            ps.nome = sc.next();
+            System.out.print("Digite sua idade: ");
+            ps.idade = sc.nextInt();
+            System.out.print("Digite o seu peso: ");
+            ps.peso = sc.nextDouble();
+            System.out.print("Digite a sua altura: ");
+            ps.altura = sc.nextDouble();
+            System.out.print("Digite o genêro 'm' de masculino ou 'f' para feminino: ");
+            ps.genero = sc.next().charAt(0);
+            System.out.print("Tem convenio?. Digite 'sim' ou 'não': ");
+            tem_convenio = sc.next();
+            if (tem_convenio == "sim") {
+                System.out.print("Digite o nome do convênio: ");
+                ps.convenio = sc.next();
+            }else{
+                break;
+            }
+            System.out.print("Deseja cadastrar outro paciente (s/n): ");
+            resp = sc.next().charAt(0);
+        }while (resp != 'n');
+
+        sc.close();
+    }
+}
